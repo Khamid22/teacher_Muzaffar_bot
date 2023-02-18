8a18b15e505f532f77eb3fe697db8b81b80fcadb
Khamidullo <mukhammadjonovxamidullo@gmail.com>
2023-02-18 14:42:47 +0100

    Connected to the postgresql

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,13 +9,13 @@ from data.config import ADMINS
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     try:
-        db.add_user(telegram_id=message.from_user.id,
+        user = await db.add_user(telegram_id=message.from_user.id,
                     full_name=message.from_user.full_name,
                     username=message.from_user.username)
     except asyncpg.exceptions.UniqueViolationError:
-        user = await db.select_user(telegram_id = message.from_user.id)
+      user = await db.select_user(telegram_id=message.from_user.id)
     await message.answer(f"Hi, {message.from_user.full_name}!", reply_markup=menu_keyboard)
 
-    count = await db.count_user()
+    count = await db.count_users()
     msg = f"{user[1]} has been added to the database.\nNumber of users: {count}."
     await bot.send_message(chat_id=ADMINS[0], text=msg)
